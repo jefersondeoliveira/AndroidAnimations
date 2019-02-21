73d768243d2e6bd3be22b77fbf44f90cfc501ddd
Jeferson Lopes <jeferson.lopes@zup.com.br>
2019-02-21 07:18:16 -0300

    add task solution

--- AnimationResources/app/src/main/java/com/example/jefersonlopes/animationresources/MainActivity.java
+++ AnimationResources/app/src/main/java/com/example/jefersonlopes/animationresources/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jefersonlopes.animationresources;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button start;
     ImageView image;
+    ConstraintLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         start = findViewById(R.id.start);
         image = findViewById(R.id.image);
+        root = findViewById(R.id.root);
 
 
         start.setOnClickListener(new View.OnClickListener() {
@@ -28,9 +31,11 @@ public class MainActivity extends AppCompatActivity {
 
                 Animation animation1 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animate_one);
                 Animation animation2 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animate_two);
+                Animation animation3 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animate_tree);
 
                 v.startAnimation(animation1);
                 image.startAnimation(animation2);
+                root.startAnimation(animation3);
 
             }
         });
